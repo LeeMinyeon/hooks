335b5b0fb6d324221ef790a439957f6a2a4df6c9
LeeMinyeon <alsdus1412@gmail.com>
2021-04-15 10:20:43 +0900

    Initial commit

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,31 @@
+export const useConfirm = (message = "", onComfirm,onCancel) => {
+
+    if (!onComfirm||typeof onComfirm !== "function") {
+    
+    return;
+    
+    }
+    
+    if (onCancel&&typeof onCancel !== "function") {
+    
+        return;
+        
+        }
+    const confirmAction = () => {
+    
+    
+    if (confirm(message)) {
+    
+    onComfirm();
+    
+    }else{
+    
+    onCancel();
+    
+    }
+    
+    };
+    
+    return confirmAction;
+    
+    };
